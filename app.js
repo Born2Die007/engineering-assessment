1312bcaf03a76890b48ad0f6d8c53ae3da1cf715
Ali Kasmani <kasmanialisaad@gmail.com>
2024-05-09 16:48:25 -0500

    CSV Parser helper

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const app = require('express')();
 const port = process.env.PORT || 3131;
+const { parseCSV } = require('./helpers/parseCSV');
 
+const foodTrucks = parseCSV('./Mobile_Food_Facility_Permit.csv');
 
 app.get('/', (req, res) => {
     res.send('Hello World');
---
+++ helpers/parseCSV.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// Function to parse CSV data
+function parseCSV(filePath, quoteChar = '"', delimiter = ',') {
+
+  const data = fs.readFileSync(filePath, 'utf8');
+  const rows = data.split("\n");
+  const headers = rows[0].split(",");
+
+  const regex = new RegExp(`\\s*(${quoteChar})?(.*?)\\1\\s*(?:${delimiter}|$)`, 'gs');
+
+  const match = line => [...line.matchAll(regex)]
+    .map(m => m[2]) 
+    .slice(0, -1); 
+
+  let lines = data.split('\n');
+  const heads = headers ?? match(lines.shift());
+  lines = lines.slice(1);
+  
+  return lines.map(line => {
+    return match(line).reduce((acc, cur, i) => {
+      // replace blank matches with `null`
+      const val = cur.length <= 0 ? null : Number(cur) || cur;
+      const key = heads[i] ?? `{i}`;
+      return { ...acc, [key]: val };
+    }, {});
+  });
+
+}
+
+module.exports = { parseCSV };
